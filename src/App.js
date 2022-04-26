3c7426f65383cc081bdac5082ca50d96677c196b
Chema Torres <chemaaa94@gmail.com>
2022-04-25 21:37:41 -0500

    useState & useEffects

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
+import React, { useEffect, useState } from "react";
 import Navbar from "./components/Navbar";
 
-function App () {
-return(
-  <Navbar/>
-)
+function App() {
 
+  const [characters, setCharacters] = useState([]);
+
+  const initialUrl = "https://rickandmortyapi.com/api/character";
+
+  const fetchCharters = (url) => {
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => setCharacters(data.results))
+      .catch((error) => console.log(error));
+  };
+
+  useEffect(() => {
+    fetchCharters(initialUrl);
+  }, []);
+
+
+  return <Navbar brand="Ricky and Morty App" />;
 }
 
 export default App;
