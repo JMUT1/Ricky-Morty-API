9c13259ec482a6a6d5f88a5d37ad623839d0adff
Chema Torres <chemaaa94@gmail.com>
2022-04-25 19:35:33 -0500

    navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from "react";
+import App from "../App";
+
+function Navbar ()  {
+  <nav className="navbar navbar-dark bg-dark">
+    <div className="container">
+      <a className="navbar-brand text-uppercase" href="/">
+        {}
+      </a>
+    </div>
+  </nav>
+};
+
+export default App
