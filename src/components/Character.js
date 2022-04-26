fd2ef8c7e69f4be01fb6474052981241cc2d7b6d
Chema Torres <chemaaa94@gmail.com>
2022-04-25 21:58:07 -0500

    informacion card ricky and morty

--- src/components/Character.js
+++ src/components/Character.js
@@ -3,8 +3,14 @@ const Character = ({ characters }) => {
     <div className="row">
       {characters.map((item, index) => (
         <div key={index} className="col">
-          <div className="card">
-              <img src={item.image}/>
+          <div className="card" style={{ minWidth: "200px" }}>
+            <img src={item.image} />
+            <div className="card-body">
+              <h5 className="card-title">{item.name}</h5>
+              <hr/>
+              <p>Location: {item.location.name}</p>
+              <p>Species: {item.species}</p>
+            </div>
           </div>
         </div>
       ))}
